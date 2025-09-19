7667974285e511397080ebdfe69c776cf90bebab
Marwan <2017159@brunel.ac.uk>
2025-09-19 17:55:29 +0100

    Update TradeStrategies.tsx

--- src/app/dashboard/components/lists/TradeStrategies.tsx
+++ src/app/dashboard/components/lists/TradeStrategies.tsx
@@ -3,12 +3,27 @@
 import React from "react";
 import TradeList from "./TradeList";
 import StrategiesList from "./StrategiesList";
+import { useRouter } from "next/navigation";
 
 export default function TradeStrategies({ userId }: { userId: string }) {
+  const router = useRouter();
+
   return (
-    <div className="flex justify-between w-[100%] max-w-350">
-      <TradeList userId={userId} />
-      <StrategiesList userId={userId} />
+    <div className="w-[100%] max-w-350">
+      <div className="flex justify-between w-[100%] max-w-350">
+        <TradeList userId={userId} />
+        <StrategiesList userId={userId} />
+      </div>
+      <div className="text-center text-sm text-white/60 mt-4">
+        Click{" "}
+        <span
+          className="underline cursor-pointer"
+          onClick={() => router.push("/settings")}
+        >
+          here
+        </span>{" "}
+        to import all trades.
+      </div>
     </div>
   );
 }
