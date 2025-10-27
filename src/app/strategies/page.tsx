a0eed8ecd70b3125f31b06bb642e5a6c9583cca2
Marwan <2017159@brunel.ac.uk>
2025-10-27 15:06:43 +0000

    Changed the layout of the strategies

--- src/app/strategies/page.tsx
+++ src/app/strategies/page.tsx
@@ -14,27 +14,49 @@ function page() {
     };
     return (
       <div
-        className="w-[200px] h-[100px] bg-[#1c1c22] flex items-center justify-center 
+        className="w-[400px] h-[50px] bg-[#16151C] flex items-center px-3 justify-between
         rounded-lg border-1 border-white/10 cursor-pointer transition duration-100 hover:border-white/60"
         onClick={() => handleRoute(path)}
       >
         <div className="text-center">{title}</div>
+        <i className="fa-solid fa-chevron-right"></i>
       </div>
     );
   };
 
   return (
     <div className="p-10 flex flex-col items-center h-[100vh] justify-center">
-      <div className="grid grid-cols-3 gap-5">
-        {strategies.map((strategy, index) => {
-          return (
-            <Card
-              title={strategy.title}
-              path={`/strategies/${strategy.slug}`}
-              key={index}
-            />
-          );
-        })}
+      <div className="grid grid-cols-2 gap-5">
+        <div className="flex flex-col gap-4">
+          <div className="text-green-500 text-xl text-center font-bold">
+            CALL
+          </div>
+          {strategies
+            .filter((s) => s.options === "CALL")
+            .map((s, index) => {
+              return (
+                <Card
+                  title={s.title}
+                  path={`/strategies/${s.slug}`}
+                  key={index}
+                />
+              );
+            })}
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="text-red-500 text-xl text-center font-bold">PUT</div>
+          {strategies
+            .filter((s) => s.options === "PUT")
+            .map((s, index) => {
+              return (
+                <Card
+                  title={s.title}
+                  path={`/strategies/${s.slug}`}
+                  key={index}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
